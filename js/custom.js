19491912baaabafc0139606f023bd6e3e55cfae7
mantonic002 <mantonic002@gmail.com>
2025-10-06 15:00:58 +0200

    index and contact pages

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').delay(100).slideUp('slow'); // set duration in brackets    
+    });
+
+    // NAVBAR
+    $(".navbar").headroom();
+
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $('.slick-slideshow').slick({
+      autoplay: true,
+      autoplaySpeed: 4000,
+      infinite: true,
+      arrows: false,
+      fade: true,
+      dots: true,
+    });
+
+    $('.slick-testimonial').slick({
+      arrows: false,
+      dots: true,
+    });
+    
+  })(window.jQuery);
